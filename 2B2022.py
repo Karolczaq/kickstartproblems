82a79cab397925da2260a5a5817c5f270bd870fd
Karol <theorangebox3@gmail.com>
2022-07-12 16:12:17 +0200

    Create 2B2022.py

---
+++ 2B2022.py
@@ -0,0 +1,17 @@
+# This solutions exceeds the time limit for test set 2
+t = int(input())
+for i in range(1,t+1):
+    wynik = 1 # a/a = 1 which is a palindrome
+    a = int(input())
+    if a == 1:
+        wynik = 1
+    else:
+        if str(a) == str(a)[::-1]:
+            wynik += 1
+        k = 2
+        while k <= a/2:
+            if a%k == 0:
+                if str(int(a/k)) == str(int(a/k))[::-1]:
+                    wynik+=1
+            k+=1
+    print("Case #{}: {}".format(i,wynik))
